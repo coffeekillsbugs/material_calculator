f77b630be698922fe7ffc5d544852e0fa3dbebc9
Nihar Lekhade <arthurexcalibur9@protonmail.com>
2020-02-22 13:46:24 +0530

    version change Button layout added

--- lib/Screens/calculator.dart
+++ lib/Screens/calculator.dart
@@ -15,32 +15,50 @@ class _CalculatorState extends State<Calculator> {
       home: Stack(
         children: <Widget>[
           // * Calculator body *//
-          Column(
-            children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height * 0.375,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: notSoWhite,
-                ),
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).padding.vertical + 10,
-                    left: 10.0,
-                    right: 10.0,
+          Scaffold(
+                      body: Column(
+              children: <Widget>[
+                // * Display * //
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.375,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: notSoWhite,
                   ),
-                  child: Column(
-                    children: <Widget>[],
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).padding.vertical + 10,
+                      left: 10.0,
+                      right: 10.0,
+                    ),
+                    child: Column(
+                      children: <Widget>[],
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.625,
-                decoration: BoxDecoration(
-                  color: baffllingBlue,
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.625,
+                  decoration: BoxDecoration(
+                    color: baffllingBlue,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Container(
+                      //color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          ColOne(),
+                          ColTwo(),
+                          ColThree(),
+                          ColFour(),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
           // * Menu Icon * //
           Padding(
@@ -50,7 +68,7 @@ class _CalculatorState extends State<Calculator> {
             ),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(30.0),
-                          child: Material(
+              child: Material(
                 color: Colors.transparent,
                 child: InkWell(
                   splashColor: placidOrange,
@@ -70,6 +88,222 @@ class _CalculatorState extends State<Calculator> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ColOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Button(
+          parentColor: placidOrange,
+          parentText: 'AC',
+        ),
+        Button(
+          parentColor: otherBlue,
+          parentText: 'x!',
+        ),
+        Button(
+          parentColor: Colors.transparent,
+          parentText: '7',
+        ),
+        Button(
+          parentColor: Colors.transparent,
+          parentText: '4',
+        ),
+        Button(
+          parentColor: Colors.transparent,
+          parentText: '1',
+        ),
+        Button(
+          parentColor: otherBlue,
+          parentText: 'e',
+        ),
+      ],
+    );
+  }
+}
+
+class ColTwo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Button(
+          parentColor: placidOrange,
+          parentText: 'C',
+        ),
+        Button(
+          parentColor: otherBlue,
+          parentText: 'x^y',
+        ),
+        Button(
+          parentColor: Colors.transparent,
+          parentText: '8',
+        ),
+        Button(
+          parentColor: Colors.transparent,
+          parentText: '5',
+        ),
+        Button(
+          parentColor: Colors.transparent,
+          parentText: '2',
+        ),
+        Button(
+          parentColor: Colors.transparent,
+          parentText: '0',
+        ),
+      ],
+    );
+  }
+}
+
+class ColThree extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Button(
+          parentColor: otherBlue,
+          parentText: '%',
+        ),
+        Button(
+          parentColor: otherBlue,
+          parentText: 'x',
+        ),
+        Button(
+          parentColor: Colors.transparent,
+          parentText: '9',
+        ),
+        Button(
+          parentColor: Colors.transparent,
+          parentText: '6',
+        ),
+        Button(
+          parentColor: Colors.transparent,
+          parentText: '3',
+        ),
+        Button(
+          parentColor: otherBlue,
+          parentText: '.',
+        ),
+      ],
+    );
+  }
+}
+
+class ColFour extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Button(
+          parentColor: otherBlue,
+          parentText: '/',
+        ),
+        Button(
+          parentColor: otherBlue,
+          parentText: 'X',
+        ),
+        Button(
+          parentColor: otherBlue,
+          parentText: '-',
+        ),
+        Button(
+          parentColor: otherBlue,
+          parentText: '+',
+        ),
+        ButtonTwo(
+          parentColor: placidOrange,
+          parentText: '=',
+        ),
+        // Button(
+        //   parentColor: placidOrange,
+        //   parentText: '=',
+        // ),
+      ],
+    );
+  }
+}
+
+class Button extends StatelessWidget {
+  final parentColor;
+  final parentText;
+
+  Button({this.parentColor, this.parentText});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: otherBlue,
+      onTap: () {
+        //TODO : Add functioning
+      },
+          child: Container(
+        width: 80.0,
+        decoration: BoxDecoration(
+          color: parentColor,
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 22.0, vertical: 12.0),
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              parentText,
+              style: TextStyle(
+                color: notSoWhite,
+                fontFamily: 'Righteous',
+                fontSize: 30.0,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonTwo extends StatelessWidget {
+  final parentColor;
+  final parentText;
+
+  ButtonTwo({this.parentColor, this.parentText});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        //TODO : Add functioning
+      },
+          child: Container(
+        width: 80.0,
+        height: 115.0,
+        decoration: BoxDecoration(
+          color: parentColor,
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 22.0, vertical: 12.0),
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              parentText,
+              style: TextStyle(
+                color: notSoWhite,
+                fontFamily: 'Righteous',
+                fontSize: 30.0,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
